ed4cb38b979a2a80db3175e56ce5ce0828efbe70
David Quarel <u5354853@anu.edu.au>
2015-06-08 16:52:01 +1000

    I fixed zoom and added height/width back again

--- mandelbrot.c
+++ mandelbrot.c
@@ -18,14 +18,15 @@ const double BOUND = 4; //square roots are expensive
 int ITER_MAX = 255;
 int COLOR_STEP = 11;
 
-int SIZE = 1024;
-int width = 1024;
-int height = 1024;
+//int SIZE = 1024;
+int WIDTH = 1024;
+int HEIGHT = 1024;
 
 double CENTER_X = -0.5;
 double CENTER_Y = 0;
 double RADIUS = 1;
-double SCALE_FACTOR = 2*2/ (double)1024 + 1;
+double SCALE_FACTOR_X = 2*2/ (double)1024 + 1;
+double SCALE_FACTOR_Y = 2*2/ (double)1024 + 1;
 
 typedef struct{
 	double re;
@@ -43,8 +44,8 @@ complex make_complex(double real, double imag){
 complex coord_to_complex(int x, int y){
 	double new_x;
 	double new_y;
-	new_x = (double)x * 2 * RADIUS / (double)SIZE + CENTER_X - RADIUS; //scale [0,SIZE] to [X-R,X+R]
-	new_y = (double)y * 2 * RADIUS / (double)SIZE - CENTER_Y - RADIUS; //sclae [0,SIZE] to [Y-R,Y+R]
+	new_x = (double)x * 2 * RADIUS / (double)WIDTH + CENTER_X - RADIUS; //scale [0,SIZE] to [X-R,X+R]
+	new_y = (double)y * 2 * RADIUS / (double)HEIGHT - CENTER_Y - RADIUS; //sclae [0,SIZE] to [Y-R,Y+R]
 	//new_x = (double)x * 4 / SIZE - 2; //scale [0,SIZE] to [-2,2]  
 	//new_y = (double)y * 4 / SIZE - 2; //scale [0,SIZE] to [-2,2]
 	return make_complex(new_x,new_y);
@@ -62,7 +63,6 @@ double mag_sq(complex a){
 	return (a.re*a.re + a.im*a.im);
 }
 
-
 typedef struct{
 	char r;
 	char g;
@@ -76,8 +76,8 @@ color make_color(int red, int green, int blue){
 
 
 void mandelbrot_bw(){ //0 if in set, 1 else, B&W
-	char *image = (char *)malloc(SIZE * SIZE * sizeof(char) * 2 + SIZE + 16);
-	char *start = image + sprintf(image, "P1\n%d %d\n", SIZE, SIZE);
+	char *image = (char *)malloc(WIDTH * HEIGHT * sizeof(char) * 2 + HEIGHT + 16);
+	char *start = image + sprintf(image, "P1\n%d %d\n", WIDTH, HEIGHT);
 	char *current;
 	
 	int x;
@@ -86,9 +86,9 @@ void mandelbrot_bw(){ //0 if in set, 1 else, B&W
 	#pragma omp parallel private(x, y, c, current) shared(image)
 	{
 		#pragma omp for
-		for(y=0; y < SIZE; y++){ 
-			current = start + (y * SIZE * 2 + y);
-			for(x=0; x < SIZE; x++){
+		for(y=0; y < HEIGHT; y++){ 
+			current = start + (y * HEIGHT * 2 + y);
+			for(x=0; x < WIDTH; x++){
 				c = coord_to_complex(x,y);
 				int iter;
 
@@ -118,8 +118,8 @@ void mandelbrot_bw(){ //0 if in set, 1 else, B&W
 }
 
 void mandelbrot_gs(){ //8-bit binary greyscale binary
-	char *image = (char *)malloc(SIZE * SIZE * sizeof(char) + 50);
-	int pre = sprintf(image, "P5\n%d %d\n255\n",SIZE,SIZE);
+	char *image = (char *)malloc(WIDTH * HEIGHT * sizeof(char) + 50);
+	int pre = sprintf(image, "P5\n%d %d\n255\n",WIDTH,HEIGHT);
 
 	char *start = image + pre;
 	char *current;
@@ -130,9 +130,9 @@ void mandelbrot_gs(){ //8-bit binary greyscale binary
 	#pragma omp parallel private(x, y, c, current) shared(image)
 	{ 
 		#pragma omp for
-		for(y=0; y < SIZE; y++){ 
-			current = start + (y * SIZE);
-			for(x=0; x < SIZE; x++){
+		for(y=0; y < HEIGHT; y++){ 
+			current = start + (y * HEIGHT);
+			for(x=0; x < WIDTH; x++){
 				c = coord_to_complex(x,y);
 				int iter;
 
@@ -153,22 +153,22 @@ void mandelbrot_gs(){ //8-bit binary greyscale binary
 			}
 		}
 	}
-	write(1, image, pre + SIZE * SIZE);
+	write(1, image, pre + HEIGHT * WIDTH);
 	free(image);
 	return;
 }
 
 void mandelbrot_col(){ //24-bit, colour changes with iteration depth, binary
 	//color world[SIZE*SIZE];
-	color *world = (color *)malloc(SIZE * SIZE * sizeof(color));
-	printf("P6\n%d %d\n255\n",SIZE,SIZE); //P6, colour binary file
+	color *world = (color *)malloc(HEIGHT * WIDTH * sizeof(color));
+	printf("P6\n%d %d\n255\n",WIDTH,HEIGHT); //P6, colour binary file
 	int x;
 	int y;
 	complex c;
 	int iter_max_col;
 	for(iter_max_col=1; iter_max_col<ITER_MAX; iter_max_col++){
-		for(y=0; y < SIZE; y++){ 
-			for(x=0; x < SIZE; x++){
+		for(y=0; y < HEIGHT; y++){ 
+			for(x=0; x < WIDTH; x++){
 				c = coord_to_complex(x,y);
 				complex z = c_zero;
 				int iter;
@@ -180,15 +180,15 @@ void mandelbrot_col(){ //24-bit, colour changes with iteration depth, binary
 				}
 				if(!iter){
 					char val = iter_max_col*COLOR_STEP; //choose prime numbers for pretty colours
-					world[SIZE*y+x] = make_color(val,val,val*2);
+					world[WIDTH*y+x] = make_color(val,val,val*2);
 				}
 			}
 		}
 	}
 
-	for(y=0; y < SIZE; y++){ 
-		for(x=0; x < SIZE; x++){
-			color cell = world[SIZE*y+x];
+	for(y=0; y < HEIGHT; y++){ 
+		for(x=0; x < WIDTH; x++){
+			color cell = world[WIDTH*y+x];
 			printf("%c%c%c",cell.r,cell.g,cell.b);
 		}
 	}
@@ -200,10 +200,10 @@ void mandelbrot_col(){ //24-bit, colour changes with iteration depth, binary
 }
 
 void print_help(){
-	printf("usage: mandelbrot [-h | --help] [-d D] [-f b|g|c] [-s S] [-i I] [-z X Y R]\n"
+	printf("usage: mandelbrot [-h | --help] [-d W H] [-f b|g|c] [-s S] [-i I] [-z X Y R]\n"
 		"-h | --help: Print this help\n"
 		"-d: dimensions of image, default 1024x1024\n"
-		"D: SIZE/SIZE of image\n"
+		"W/H: WIDTH/HEIGHT of image\n"
 		"\n"
 		"-f: choose format, default greyscale\n"
 		"b: 1-bit B&W, ASCII output\n"
@@ -225,7 +225,7 @@ void print_help(){
 }
 
 void print_usage(){
-	printf("usage: mandelbrot [-h | --help] [-d D] [-f b|g|c] [-step S] [-i I] [-z X Y R]\n");
+	printf("usage: mandelbrot [-h | --help] [-d W H] [-f b|g|c] [-step S] [-i I] [-z X Y R]\n");
 	return;
 }
 
@@ -240,8 +240,9 @@ int main(int argc, char *argv[]){
     	}
     	//read dimension
     	else if(input_eq("-d")){ 
-    		sscanf(argv[i+1],"%d",&SIZE);
-    		i++;
+    		sscanf(argv[i+1],"%d",&WIDTH);
+    		sscanf(argv[i+2],"%d",&HEIGHT);
+    		i+=2;
     	}
     	//read format
     	else if(input_eq("-f")){ 
@@ -274,7 +275,8 @@ int main(int argc, char *argv[]){
     }//for(i=1..)
     
     //End Handle Input
-    SCALE_FACTOR = 2*RADIUS/ (double)SIZE + 1;
+    SCALE_FACTOR_X = 2*RADIUS/ (double)WIDTH + 1;
+    SCALE_FACTOR_Y = 2*RADIUS/ (double)HEIGHT + 1;
 
 	if(format == 'b'){ //1-bit black and white mandelbrot
 		mandelbrot_bw();
